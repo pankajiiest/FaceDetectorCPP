b3a05bcabeb39d87db84f1e6d866fb0fd6e63f73
Pankaj Kumar <pankajkr8006@gmail.com>
2024-10-07 18:34:56 +0530

    Initial Commit

---
+++ MyFaceDetectorCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyFaceDetectorCPP
+//
+//  Created by Pankaj Kumar on 07/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
